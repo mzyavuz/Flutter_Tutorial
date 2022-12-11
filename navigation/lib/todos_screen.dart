46468b3fb2afdc3d29c0dc3786e4c8dafa75bb2c
mzyavuz <melikezeynep_97@hotmail.com>
2022-12-11 22:44:40 +0300

    send data to a new screen

---
+++ navigation/lib/todos_screen.dart
@@ -0,0 +1,44 @@
+
+import 'package:flutter/material.dart';
+
+import 'Todo.dart';
+import 'details_screen.dart';
+
+class TodosScreen extends StatelessWidget {
+  const TodosScreen({super.key, required this.todos});
+
+  final List<Todo> todos;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todos'),
+      ),
+      body: ListView.builder(
+        itemCount: todos.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(todos[index].title),
+            // When a user taps the ListTile, navigate to the DetailScreen.
+            // Notice that you're not only creating a DetailScreen, you're
+            // also passing the current todo through to it.
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const DetailScreen(),
+                  // Pass the arguments as part of the RouteSettings. The
+                  // DetailScreen reads the arguments from these settings.
+                  settings: RouteSettings(
+                    arguments: todos[index],
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
